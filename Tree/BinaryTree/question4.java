8d4ee55b8f67e729a7952c236b404580272111e3
yuchenwang2011 <yuchen.wang2011@gmail.com>
2015-10-03 22:36:12 -0400

    add question 4 and it's answer

---
+++ Tree/BinaryTree/question4.java
@@ -0,0 +1,13 @@
+public BinaryTreeNode getCommandAncestor(BinaryTreeNode root, int A, int B) {
+  while(root != null) {
+    int value = root.getKey();
+    if (value < A.getValue() && value < B.getValue()) {
+       root = root.getLeftChild();
+    } elseif (value > A.getValue() && value > B.getValue()) {
+       root = root.getRightChild();
+    } else {
+       return root;
+    }
+  }
+  return null;
+}
