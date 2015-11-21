a7a3c523beebbb6ea6ea33a822e45cea7140dd96
yuchenwang2011 <yuchen.wang2011@gmail.com>
2015-11-21 11:57:57 -0500

    Best Sorting Algorithm question1

---
+++ Sorting/question1.java
@@ -0,0 +1,9 @@
+1.What do we know about the data? 
+  a.Is the data already sorted or mostly sorted?
+  b.How large are the data sets likely to be?
+  c.Can there be duplicate key values?
+2.What are the requirements for the sort?
+  a. Do you want to optimize for best-case, worst-case, or averate-case performance?
+  b. Does the sort need to be stable?
+3.What do we know about the system?
+  a. Is the largest data set to be sorted smaller than, the same size as, or larger than available memory? 
