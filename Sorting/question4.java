563cab2c7991c49199215c58d4ed1523092ddca5
yuchenwang2011 <yuchen.wang2011@gmail.com>
2015-11-21 20:06:54 -0500

    Question 4

---
+++ Sorting/question4.java
@@ -0,0 +1,4 @@
+Such as Arrays.sort() in Java. 
+These routines typically employ merge sort (if stability is important) 
+or quicksort (if it isn’t) for most datasets, often switching to insertion sort 
+for very small datasets (typically n less than approximately 10).
