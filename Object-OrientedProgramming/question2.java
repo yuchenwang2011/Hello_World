03aecaf0e3a36acf3bcb51b23ab6ab6f934f8182
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-01-09 16:22:52 -0500

    Virtual Method

---
+++ Object-OrientedProgramming/question2.java
@@ -0,0 +1,5 @@
+All methods in java are virtual by default. 
+That means that any method can be overridden when used in inheritance, 
+unless that method is declared as final or static.
+The equivalent to a pure virtual method is an abstract method.
+Nonstatic Java methods are always virtual, but not static methods.
