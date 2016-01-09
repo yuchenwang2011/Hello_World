3aef5cabc6ae39b2e5c58870ec84f5b00f03c583
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-01-09 15:33:54 -0500

    Diff between interface and abstract class

---
+++ Object-OrientedProgramming/question1.java
@@ -0,0 +1,17 @@
+An interface declares a set of related methods, outside of any class.
+
+An abstract class is an incomplete class definition that declares but does not define all its
+methods.
+========
+An interface defines an application programming interface (API) that is independent of any class hierarchy. 
+Interfaces are particularly important in languages that support only single inheritance, 
+in which classes can inherit only one base class. 
+A class that implements an interface must implement all the methods declared in the interface, and a class can implements
+multiple interfaces.
+
+An abstract class is a proper class: Unlike a concrete (nonabstract) class, some of its behaviors are deliberately left to be defined by its own subclasses. 
+Abstract classes cannot be instantiated because of this — only instances of concrete subclasses can be created.
+========
+To Be Continued..
+
+
