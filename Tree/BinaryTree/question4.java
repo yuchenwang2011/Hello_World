43fb1ebf2710a393aeddc82d25f5ba2052e2a7aa
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-02-15 21:35:20 -0500

    Update question4.java

--- Tree/BinaryTree/question4.java
+++ Tree/BinaryTree/question4.java
@@ -2,9 +2,9 @@ public BinaryTreeNode getCommonAncestor(BinaryTreeNode root, int A, int B) {
   while(root != null) {
     int value = root.getKey();
     if (value < A.getValue() && value < B.getValue()) {
-       root = root.getLeftChild();
-    } elseif (value > A.getValue() && value > B.getValue()) {
        root = root.getRightChild();
+    } elseif (value > A.getValue() && value > B.getValue()) {
+       root = root.getLeftChild();
     } else {
        return root;
     }
