5a7aaef6d111cf13b7ee4cd64b99ddf0c07eed27
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-07-19 11:25:43 -0400

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,37 +1,24 @@
-import java.util.Arrays;
 //Selection sort does have the advantage that it requires at most n – 1 swaps. 
 //In situations in which moving data elements is more expensive than comparing them, 
 //selection sort may perform better than other algorithms.
-public class SelectionSorting {
-    int[] selectionSort(int[] data) {
-       sortProcess(data, 0); 
-       return data;
-    }
-  
-    void sortProcess(int[] data, int start) {
-      if (start < data.length) {
-      //first find the minimum of this recursion
-      int minimumPosition = start;
-      for(int i = start; i < data.length; i++) {
-        if ( data[i] < data[minimumPosition]) {
-          minimumPosition = i;
-        }
-      }
-      //second swap the found minimum and the element at the start position
-      int tmp = data[start];
-      data[start] = data[minimumPosition];
-      data[minimumPosition] = tmp;
-      
-      //recursion again
-      sortProcess(data, start+1);
-      }
-      
-    }
-  
-    public static void main(String[] args) {
-      SelectionSorting mySelect = new SelectionSorting();
-      int[] testInt = new int[] {3,1,123,4,6,8,9,3};
-      System.out.println(Arrays.toString(mySelect.selectionSort(testInt)));
-    }
+//https://en.wikipedia.org/wiki/File:Selection-Sort-Animation.gif
+//https://github.com/szpssky/sort-tool/raw/master/animation/Selection_sort_animation.gif
+//Average, best, worst complexity are all O(n^2), memory O(1)
+//http://javahungry.blogspot.com/2013/06/java-sorting-program-code-selection-sort.html
+
+public void selectionSort(int[] nums){
+	for(int i = 0; i < nums.length - 1; i++){
+		int minIdx = i;
+		for(int j = i + 1; j < nums.length; j++){
+			if(nums[j] < nums[minIdx]) minIdx =j;
+		}
+		swap(nums, i, minIdx);
+	}
+
 }
 
+public void swap(int[] nums, int i, int j){
+	int tmp = nums[i];
+	nums[i] = nums[j];
+	nums[j] = tmp;
+}
