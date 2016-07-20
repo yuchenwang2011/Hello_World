c1a918d693f5399691540e5214961964118e6a1f
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-07-20 10:30:31 -0400

    Update InsertionSorting.java

--- Sorting/InsertionSorting.java
+++ Sorting/InsertionSorting.java
@@ -1,24 +1,19 @@
 //http://i.stack.imgur.com/CK6vC.jpg
-import java.util.Arrays;
-public class InsertionSorting {
-    
-    int[] insertSorting(int[] data) {
-       for (int i = 1; i < data.length; i++) {    
-            for (int j = 0; j < i; j++) {  
-                 if (data[j] >= data[i]) {
-                     int tmp = data[i];
-               System.arraycopy(data, j, data, j+1, i-j);
-               data[j] = tmp;
-                 }
+//https://github.com/szpssky/sort-tool/blob/master/animation/Insertion_sort_animation.gif
+//http://www.java2novice.com/java-sorting-algorithms/insertion-sort/
+The best case when the array is already sorted, so O(n)
+The average case is O(n^2)
+The worst case is the list is already reverse order sorted, so O(n^2)
+public void insertionSort(int[] nums){
+    for(int i = 1; i < nums.length; i++){
+        int tmp;
+        for(int j = i; j >= 0; i--){
+            if(nums[j] < nums[j - 1]) {
+                tmp = nums[j-1];    //it's like bubble sorting here
+                nums[j - 1] = nums[j];
+                nums[j] = tmp;
             }
-       }
-     return data;
-    }
-  
-    public static void main(String[] args) {
-        InsertionSorting myInsert = new InsertionSorting();
-        int[] testInt = new int[] {4,1,3,6,9,12,4,2};
-        System.out.println(Arrays.toString(myInsert.insertSorting(testInt)));
+        }
     }
 }
 
