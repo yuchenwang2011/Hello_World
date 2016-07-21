c9b619d08651e7f6e47cee7b1db8fc464ddd178a
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-07-21 10:20:19 -0400

    Update QuickSort.java

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,106 +1,26 @@
 //this is the example, mine is the second one
-public class QuickSort {
-  public static void main(String[] args) {
-    int[] x = { 9, 2, 4, 7, 3, 7, 10 };
-    System.out.println(Arrays.toString(x));
- 
-    int low = 0;
-    int high = x.length - 1;
- 
-    quickSort(x, low, high);
-    System.out.println(Arrays.toString(x));
-  }
- 
-  public static void quickSort(int[] arr, int low, int high) {
-    if (arr == null || arr.length == 0)
-      return;
- 
-    if (low >= high)
-      return;
- 
-    // pick the pivot
-    int middle = low + (high - low) / 2;
-    int pivot = arr[middle];
- 
-    // make left < pivot and right > pivot
-    int i = low, j = high;
-    while (i <= j) {
-      while (arr[i] < pivot) {
-        i++;
-      }
- 
-      while (arr[j] > pivot) {
-        j--;
-      }
- 
-      if (i <= j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        i++;
-        j--;
-      }
+//https://github.com/szpssky/sort-tool/blob/master/animation/Sorting_quicksort_anim.gif?raw=true
+//The best case and average O(nlogn), worst case O(n^2) because of each time pivot is a min or max
+//space complexity is O(logn), it's the stack space, even in the worst case.
+public void quickSort(int[] arr, int low, int high) {
+  if (arr == null || arr.length == 0 || low >= high) return;
+  int pivot = arr[low + (high - low) / 2];
+  // make left elements < pivot and right elements > pivot
+  int i = low, j = high;
+  while (i <= j) {
+    while (arr[i] < pivot) i++; //means it's good
+    while (arr[j] > pivot) j--; //means it's good
+    if (i <= j) {
+      swap(arr, i, j);
+      i++; j--;
     }
- 
-    // recursively sort two sub parts
-    if (low < j)
-      quickSort(arr, low, j);
- 
-    if (high > i)
-      quickSort(arr, i, high);
   }
+  if (j > low) quickSort(arr, low, j);
+  if (i < high) quickSort(arr, i, high);
 }
 
-import java.util.Arrays;
-public class QuickSort {
-    void quickSort(int[] data, int low, int high) {
-      if( data == null || data.length ==0) {
-        return;
-      }
-      
-      if(low >= high) {
-        return;
-      }
-      
-      int middle = low + (high - low)/2;
-      int pivot = data[middle];
-      int i = low; int j = high;
-      while(i <= j) {
-        //try to find a bigger element, can't be equal
-        while (data[i] < pivot) {  //*****Remember!!!!! here it can't be data[pivot]!! because data is changing!
-          i++;
-        }
-        while (data[j] > pivot) {  //*****Remember!!!!! here it must be a fixed pivot VALUE!!
-          j--;
-        }
-        
-        if (i <= j) {
-          int tmp = data[i];
-          data[i] = data[j];
-          data[j] = tmp;
-          i++;
-          j--;
-        }
-      }     
-      //until now, I don't need to worry about any details about the array, whatever
-      //I just need to know that all the elements before i are smaller than pivot
-      //And all the elements after j are bigger than pivot
-      //Now, all elements after j is bigger than pivot; all elements before i is smaller than pivot
-      //So that means, any elements between i and j now are same as pivot
-      //And j should be on the left, i on the right. sort the left half part and sort the right half part. 
-      if(low < j) {   //the two ifs are error tolerance
-        quickSort(data, low, j);
-      }
-      if(high > i) {
-        quickSort(data, i, high);
-      }   
-    }
-    
-    public static void main(String[] args) {
-      QuickSort mySort = new QuickSort();
-      int[] testInt = new int[] {2,2,1,3,45,6,2,-5,8,3,7};
-      mySort.quickSort(testInt, 0, testInt.length-1);
-      System.out.println(Arrays.toString(testInt));
-    }
+public void swap(int[] nums, int i, int j){
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
 }
-
