cbd59cffa8fe50638176dfcf096e29cb776df7cb
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-08-28 19:03:31 -0400

    Update DesignPattern.java

--- DesignPatterns/DesignPattern.java
+++ DesignPatterns/DesignPattern.java
@@ -137,3 +137,50 @@ public abstract class Game {
     }
     运行结果: 王宝强 参加演出
               经纪人 照顾妻子
+
+7. Strategy Mode：
+    假设现在要设计一个贩卖各类书籍的电子商务网站的购物车系统。一个最简单的情况就是把所有货品的单价乘上数量，
+    但是实际情况肯定比这要复杂。比如，本网站可能对所有的高级会员提供每本20%的促销折扣；对中级会员提供每本10%的促销折扣；
+    对初级会员没有折扣。
+    
+    public interface MemberStrategy {
+         public double calcPrice(double booksPrice);
+    }
+    public class PrimaryMemberStrategy implements MemberStrategy {
+        @Override
+        public double calcPrice(double booksPrice) {
+            System.out.println("对于初级会员的没有折扣");
+            return booksPrice;
+        }
+    }
+    public class IntermediateMemberStrategy implements MemberStrategy {
+         @Override
+         public double calcPrice(double booksPrice) {
+            System.out.println("对于中级会员的折扣为10%");
+            return booksPrice * 0.9;
+         }
+    }
+    public class AdvancedMemberStrategy implements MemberStrategy {
+        @Override
+        public double calcPrice(double booksPrice) {
+            System.out.println("对于高级会员的折扣为20%");
+            return booksPrice * 0.8;
+        }
+    }
+    public class Price {
+         private MemberStrategy strategy;
+         public Price(MemberStrategy strategy){
+            this.strategy = strategy;
+         }
+         public double quote(double booksPrice){
+           return this.strategy.calcPrice(booksPrice);
+         }
+    }
+    public class Client {
+        public static void main(String[] args) {
+             MemberStrategy strategy = new AdvancedMemberStrategy();
+             Price price = new Price(strategy);
+             double quote = price.quote(300);
+             System.out.println("图书的最终价格为：" + quote);
+        }
+    }
