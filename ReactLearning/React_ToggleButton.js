2445356c22e91d70436ba7c95b192f0f591b49ef
yuchenwang2011 <yuchen.wang2011@gmail.com>
2017-10-18 12:10:32 -0400

    React Toggle/Login button

---
+++ ReactLearning/React_ToggleButton.js
@@ -0,0 +1,32 @@
+//from https://reactjs.org/docs/handling-events.html
+//In JavaScript, class methods are not bound by default. 
+//If you forget to bind this.handleClick and pass it to onClick,
+//this will be undefined when the function is actually called
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
