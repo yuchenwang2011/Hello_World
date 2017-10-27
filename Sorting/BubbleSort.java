d30701c60eb4f97a2b538260037ac6854e0ba158
yuchenwang2011 <yuchen.wang2011@gmail.com>
2017-10-27 11:09:17 -0400

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -5,6 +5,8 @@ Best case O(n), worse case and average O(n^2), memory O(1) auxiliary
 	public void bubbleSort(int[] nums){
 		int n = nums.length;
 		for(int i = 0; i < nums.length; i++){
+		//start from 1 prevent outof idx, n - i because after first round, the last element is already the largest
+		//in the second round, the last second one is the second largest, no need to compare the last two elements
 			for(int j = 1; j < n - i; j++){
 			
 				if(nums[j - 1] > nums[j]) {
