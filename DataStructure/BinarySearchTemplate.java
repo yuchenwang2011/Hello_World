734360dd1c76a1551bdc45ac4632d674ced93663
yuchenwang2011 <yuchen.wang2011@gmail.com>
2023-09-20 00:18:31 -0700

    Update BinarySearchTemplate.java

--- DataStructure/BinarySearchTemplate.java
+++ DataStructure/BinarySearchTemplate.java
@@ -26,5 +26,5 @@ public class Solution {
   配合while终止条件start + 1 < end（相邻即退出）的赋值语句mid永远没有+1或者-1，这样不会死循环。
 4. 迭代终止时target应为start或者end中的一个。循环终止条件有两个，具体应看是找第一个还是最后一个而定。
 
-
+以后我就采用第二种，[左闭右开)的写法
 https://www.cnblogs.com/cnoodle/p/14267991.html
